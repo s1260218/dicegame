98f443a5228a14517e65c82594f4578baf0ae49e
s1260218 <s1260218@ilab2dc38.u-aizu.ac.jp>
2019-07-11 15:38:28 +0900

    initial revision

---
+++ dice.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main(void){
+  int i,j,k=0;
+
+  printf("Rolling the dice...\n");
+  srand((unsigned int)time(NULL));
+  
+  for(i=0;i<2;i++){
+    j=rand()%6+1;
+    printf("die %d: %d\n",i+1,j);
+    k+=j;
+  }
+  printf("Total value: %d\n",k);
+  
+    return 0;
+  }
